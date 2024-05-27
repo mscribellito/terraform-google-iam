b2996cdd29ec34cab320ccd7fe3c03b28dc90fe7
Michael Scribellito <mscribellito@users.noreply.github.com>
2024-05-27 15:35:37 -0400

    Refactor

--- main.tf
+++ main.tf
@@ -2,16 +2,18 @@ locals {
 
   resources = ["project", "storage_bucket"]
 
-  bindings_by_resource = { for k, v in local.resources : v => [
-    for binding in var.bindings : binding if split("=>", binding.resource[0])[0] == v
-  ] }
+  bindings_by_resource = {
+    for resource in local.resources : resource => [
+      for binding in var.bindings : binding if split("=>", binding.resource[0])[0] == resource
+    ]
+  }
 
 }
 
 module "project" {
   source = "./modules/project"
 
-  for_each = { for i, binding in local.bindings_by_resource.project : "binding-${i + 1}" => binding }
+  for_each = { for i, binding in local.bindings_by_resource.project : i => binding }
 
   bindings = each.value
 }
@@ -19,7 +21,7 @@ module "project" {
 module "storage_bucket" {
   source = "./modules/storage_bucket"
 
-  for_each = { for i, binding in local.bindings_by_resource.storage_bucket : "binding-${i + 1}" => binding }
+  for_each = { for i, binding in local.bindings_by_resource.storage_bucket : i => binding }
 
   bindings = each.value
 }
